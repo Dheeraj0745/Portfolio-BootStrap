5aade9eb5d9760390022bf313450656944c946f1
Dheeraj mishra <dheerajmishra4545@gmail.com>
2025-09-04 16:50:49 +0530

    Update dynamic.js
    Add and alert for non functional links

---
+++ dynamic.js
@@ -0,0 +1,16 @@
+
+const HireSenseD = document.querySelector(".HireSenseDemo");
+if (HireSenseD) {
+    HireSenseD.addEventListener("click", (e) => {
+        e.preventDefault();
+        alert("HireSense AI Project is under Final Stage");
+    });
+}
+
+const HireSenseR = document.querySelector(".HireSenseReview");
+if (HireSenseR) {
+    HireSenseR.addEventListener("click", (e) => {
+        e.preventDefault();
+        alert("Source Code is not available Yet");
+    });
+}
